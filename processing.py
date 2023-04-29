e1ef619ac1fbcb67e3cb5b8c27422398fb68d99e
Ryan Guo <ryanjguo310@gmail.com>
2023-04-29 03:45:26 -0400

    Updated processing.py

--- processing.py
+++ processing.py
@@ -2,6 +2,7 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
+import fitz
 
 embeddings = OpenAIEmbeddings()
 
@@ -15,4 +16,10 @@ def get_reply(query):
     return chain.run(input_documents=docs, question=query)
 
 
+def get_text():
+    text = ""
+    with fitz.open("sample.pdf") as doc:
+        for page in doc:
+            text += page.get_text(output='text')
 
+    return text
